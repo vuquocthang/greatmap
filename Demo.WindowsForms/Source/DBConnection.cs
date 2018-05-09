79a2464c35da3968cb5563b6a72c83978edadeaa
thang vu quoc <vuquocthang63@gmail.com>
2018-05-09 11:43:31 +0700

    first commit

---
+++ Demo.WindowsForms/Source/DBConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+
+namespace Demo.WindowsForms
+{
+    class DBConnection
+    {
+        public DBConnection()
+        {
+        }
+
+       
+        public static SqlConnection Instance()
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            SqlDataReader dataReader;
+            connetionString = @"Data Source=THANGVQ\SQLEXPRESS;Initial Catalog=quanlydat;User ID=root3;Password=111111";
+            sql = "SELECT * FROM users";
+            connection = new SqlConnection(connetionString);
+
+            return connection;
+        }
+
+        
+    }
+}
